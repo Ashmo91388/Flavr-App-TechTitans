732f80ddef3c3f85276b7de162eaad1ff4ff6427
cameronmcnz~clear <ashly_mossette@yahoo.com>
2023-05-20 19:09:59 -0700

    starting point

---
+++ client/src/components/Intro/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Intro = () => {
+    return (
+        //Ash write HTML/JSS, block info is in intro from//
+        <div className="col-12 col-md-10 mb-3 p-3" "border: 1px dotted rgb(26, 26, 26);"><div><h3>Welcome to Flavr!</h3><p>You need to be logged in to share your thoughts. Please <a href="/login">login</a> or <a href="/signup">signup.</a></p></div></div>
+      );
+    };
+    
+    export default Intro;
+    
