2fd5fc6f3a7ecca1a78515a66de9811038a1a6e4
unknown <michellrahman@yahoo.com>
2023-05-22 15:21:36 -0700

    added typedefs and resolvers

--- models/userSchema.js
+++ models/userSchema.js
@@ -4,12 +4,17 @@ const userSchema = new Schema({
     email: {
         type: String,
         required: true
-
-
+    },
+    username: {
+        type: String,
+        required: true
     },
     password: String
 })
 
+userSchema.methods.isCorrectPassword = async function (password) {
+    return password === this.password;
+  };
 
 const User = model('User', userSchema);
 
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
-
-const userRoutes = require('./routes/userRoutes');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./Schemas');
 
 async function main(){
 
@@ -12,24 +12,41 @@ async function main(){
 
 main().catch(err => console.log(err));
 
+
+
+
+
+// Import the ApolloServer class
+
+
+// Import the two parts of a GraphQL schema
+
+
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
 
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
 
+ 
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+};
 
-
-
-
-app.use("/api/user", userRoutes)
-
-app.get("/", (req, res)=>{
-
-    res.json({
-        message:"hello"
-    })
-})
-
-
-app.listen(port , ()=>{
-    console.log("listening on port: " , port)
-})
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
