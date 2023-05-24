de2ab59dcbbb5c11425ac63b0a3c84d776f31b37
unknown <michellrahman@yahoo.com>
2023-05-24 13:47:45 -0700

    finished resolves/typeDefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,80 +1,80 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
-const { signToken } = require('../utils/auth');
+const User = require("../models/userSchema")
+const Restaurant = require("../models/restaurantSchema")
 
 const resolvers = {
-  Query: {
-    users: async () => {
-      return User.find().populate('thoughts');
+    Query: {
+      user: async (parent, { username }) => {
+        return User.findOne({ username })
+      },
+      restaurants: async (parent, {}) => {
+        return Restaurant.find({})
+      },
+      restaurant: async (parent, {_id}) => {
+        return Restaurant.findOne({ _id: _id });
+      },
     },
-    user: async (parent, { username }) => {
-      return User.findOne({ username }).populate('thoughts');
-    },
-    thoughts: async (parent, { username }) => {
-      const params = username ? { username } : {};
-      return Thought.find(params).sort({ createdAt: -1 });
-    },
-    thought: async (parent, { thoughtId }) => {
-      return Thought.findOne({ _id: thoughtId });
-    },
-  },
-
-  Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
-    },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
-
-      if (!user) {
-        throw new AuthenticationError('No user found with this email address');
-      }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
-      const thought = await Thought.create({ thoughtText, thoughtAuthor });
-
-      await User.findOneAndUpdate(
-        { username: thoughtAuthor },
-        { $addToSet: { thoughts: thought._id } }
-      );
-
-      return thought;
-    },
-    addComment: async (parent, { thoughtId, commentText, commentAuthor }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        {
-          $addToSet: { comments: { commentText, commentAuthor } },
+  
+    Mutation: {
+      addUser: async (parent, { username, email, password }) => {
+        const user = await User.create({ username, email, password });
+        return user;
+      },
+      addRestaurant: async (parent, { name, cuisine, image, totalRating }) => {
+        const restaurant = await Restaurant.create({ name, cuisine, image, totalRating  });
+        return restaurant;
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+  
+        return user;
+      },
+      addReview: async (parent, { restaurantId, title, content, rating, userName }) => {
+        const restaurant = await Restaurant.findById(restaurantId)
+        let newRating;
+        if (!restaurant.totalRating){
+          newRating = rating   
+        } else {
+          let numReviews = restaurant.reviews.length
+          let sumOfratings = numReviews*restaurant.totalRating
+          sumOfratings += rating 
+          newRating = sumOfratings/(numReviews + 1)
+        }
+        const updatedRestaurant = await Restaurant.findByIdAndUpdate(restaurantId, {
+          totalRating: newRating,
+            $push: {
+                reviews: {
+                    title, content, rating, userName
+                }
+            },
+           
         },
         {
-          new: true,
-          runValidators: true,
-        }
-      );
+            new: true
+        });
+        
+        return updatedRestaurant;
+      },
+      
+      removeReview: async (parent, { reviewId, restaurantId }) => {
+        return Restaurant.findOneAndUpdate(
+          { _id: restaurantId },
+          { $pull: { reviews: { _id: reviewId } } },
+          { new: true }
+        );
+      },
     },
-    removeThought: async (parent, { thoughtId }) => {
-      return Thought.findOneAndDelete({ _id: thoughtId });
-    },
-    removeComment: async (parent, { thoughtId, commentId }) => {
-      return Thought.findOneAndUpdate(
-        { _id: thoughtId },
-        { $pull: { comments: { _id: commentId } } },
-        { new: true }
-      );
-    },
-  },
-};
-
-module.exports = resolvers;
+  };
+  
+  module.exports = resolvers;
