33859222ed68ab7c9caa351b8f992c6ee6ce279c
unknown <michellrahman@yahoo.com>
2023-05-26 14:50:49 -0700

    heroku stuff

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const { ApolloServer } = require('apollo-server-express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const path = require('path');
 //const {authMiddleware}
 //const { typeDefs, resolvers } = require('./schemas');
 const typeDefs = require('./schemas/typeDefs');
@@ -36,15 +37,20 @@ const server = new ApolloServer({
 });
 
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, "../client/build")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  })
+}
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
-
+  
  
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
